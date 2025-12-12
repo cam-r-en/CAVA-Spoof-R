a76070ff1e67681a895cfd34a3f25d5cbb3adedc
Camren Henderson <hendersoncamren8@gmail.com>
2025-12-11 21:42:51 -0500

    Update CORS for GitHub Pages

--- server/server.js
+++ server/server.js
@@ -12,7 +12,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://cam-r-en.github.io'],
+  credentials: true
+}));
 app.use(express.json());
 
 // MongoDB Connection
